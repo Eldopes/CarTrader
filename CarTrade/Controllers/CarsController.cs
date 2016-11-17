a84da5ce13042d6b885964dd682ecddaf5924f3e
Eugene Morgunov <evmorgunov@yahoo.com>
2016-11-17 21:35:40 +0300

    Add project files.

---
+++ CarTrade/Controllers/CarsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel; // enables the use of collections
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarTrader.Models;
+
+namespace CarTrader.Controllers
+{
+    public class CarsController : Controller
+    {
+        private CarsContext db = new CarsContext();
+
+        // GET: Cars
+        public async Task<ActionResult> ShowAll() 
+        {
+            return View(await db.Ads.ToListAsync());
+        }
+
+        // GET: Cars/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cars cars = await db.Ads.FindAsync(id);
+            if (cars == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cars);
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {         
+            return View();
+        }
+                 
+
+        // POST: Cars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,date,CarModel,price,mileage,tel,seller,description")] Cars cars) // TODO: check if correct (previously was "model")
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ads.Add(cars);
+                await db.SaveChangesAsync();
+                return RedirectToAction("ShowAll");
+            }
+
+            return View();
+        }
+
+        // GET: Cars/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cars cars = await db.Ads.FindAsync(id);
+            if (cars == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cars);
+        }
+
+        // POST: Cars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,date,CarModel,price,mileage,tel,seller,description")] Cars cars) // TODO: check if correct (previously was "model")
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cars).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(cars);
+        }
+
+        // GET: Cars/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cars cars = await db.Ads.FindAsync(id);
+            if (cars == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cars);
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Cars cars = await db.Ads.FindAsync(id);
+            db.Ads.Remove(cars);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CarTrade/Models/Cars/Cars.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity; //Access Entity framework classes
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.ComponentModel.DataAnnotations; // for validation
+/*
+ * Car Trader => MODEL CLASSES
+ * Represent data needed for displaying page with ads from DB 
+ * 
+ * */
+
+namespace CarTrader.Models
+{
+    public class Cars // fields for storing cars data 
+    {    // (auto generated) TODO: remove nullables all in this model
+        public int id { get; set; }
+
+        public DateTime? date { get; set; } = DateTime.Now; // TODO: make local instead of US
+        
+        public virtual CarModel CarModel { get; set; }  // virtual property (reference to CarModel)
+
+        
+        [Required(ErrorMessage = "Пожалуйста введите цену")]
+        public int? price { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите пробег")]
+        public int? mileage { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите телефон")]
+        public string tel { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите имя продавца")]
+        public string seller { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Пожалуйста введите текст объявления")]
+        public string description { get; set; } // textarea
+    }
+
+    public class CarModel // fields for storing car models data 
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите модель Вашего авто")]
+        public string model { get; set; }
+
+        public virtual ICollection<Cars> Vehicles { get; set; }
+    }
+    
+
+
+    public class CarsContext : DbContext // context for Entity Framework
+    {
+        public CarsContext() : base("DefaultConnection") // to use Default Connection in web.config connection strings in this db
+        { }
+        public DbSet<Cars> Ads { get; set; }
+        public DbSet<CarModel> Mdls { get; set; }
+    }
+
+
+}
